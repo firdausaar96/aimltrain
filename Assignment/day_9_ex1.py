26f44c2ff5d29435d336a721ec4d6ae29de594bd
firdausaar96 <firdausaar96@gmail.com>
2025-10-16 12:10:14 +0800

    assignment

---
+++ Assignment/day_9_ex1.py
@@ -0,0 +1,21 @@
+def find_greatest(num1, num2, num3):
+
+  if num1 >= num2 and num1 >= num3:
+    return num1
+  elif num2 >= num1 and num2 >= num3:
+    return num2
+  else:
+    return num3
+
+try:
+  number1 = float(input("Enter the first number: "))
+  number2 = float(input("Enter the second number: "))
+  number3 = float(input("Enter the third number: "))
+
+  greatest_number = find_greatest(number1, number2, number3)
+
+
+  print(f"The greatest number is: {greatest_number}")
+
+except ValueError:
+  print("Invalid input. Please enter valid numbers.")
