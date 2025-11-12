3c4153ddbbd9f3c3d3ed346271d9e23d4112c9db
firdausaar96 <firdausaar96@gmail.com>
2025-11-12 14:37:58 +0800

    day27

---
+++ Day 27/SQLQuery3 day 1.sql
@@ -0,0 +1,26 @@
+--create object objectName
+
+create database OurDb
+--create table <TableName>
+--(ColumnName DataType <constraint>,
+	----------
+--)
+use OurDb
+
+create table Student
+(SId int primary key,
+SName nvarchar(50) not null,
+SFee float not null)
+------------------------------------------------
+select * from Student
+insert into Student values (1,'Sam',5000.50)
+insert into Student values 
+(2,'Rohit',4500.25),
+(3,'Neha',5000.58),
+(4,'Ani',6000.58),
+(5,'Arisha',4000.28)
+
+select * from Student where SId<=3
+select SName from Student
+-------------------------------------------------------------------------
+
